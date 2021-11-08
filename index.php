331780f2a7e26408ffd75e2c4a5986519c6028c7
malagonius <47065208+malagonius@users.noreply.github.com>
2021-11-08 10:14:16 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
 <script type="text/javascript" src="https://form.jotform.com/jsform/213072878550358"></script>
+<style>
+    .clear{
+        position: absolute;
+        top: 590px;
+        width: 100%;
+        height: 200px;
+        left: 0px;
+        background: white;
+    }
+    @media only screen and (max-width: 911px) {
+  .clear {
+    display:none;
+  }
+}
+</style>
 <body>
-    <div>
+    <div class="clear">
     </div>
 </body>
